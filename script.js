acb72396d644ef156354381d859c69f683a48b24
Jenny Lin <misslinyang91@gmail.com>
2021-08-17 19:45:44 +1000

    bettered the active function

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-const panels = document.querySelectorAll(".panel");
+// const panels = document.querySelectorAll(".panel");
+
+// panels.forEach((panel) => {
+//   panel.addEventListener("click", () => {
+//     removeActiveClasses();
+//     panel.classList.add("active");
+//   });
+// });
+
+// function removeActiveClasses() {
+//   panels.forEach((panel) => {
+//     panel.classList.remove("active");
+//     console.log("war");
+//   });
+// }
+
+function setClickListeners() {
+  const panels = document.querySelectorAll(".panel");
+  panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+      removeActiveClass();
+      panel.classList.add("active");
+    });
+  });
+}
+
+function removeActiveClass() {
+  const activePanel = document.querySelector(".active");
+  activePanel.classList.remove("active");
+}
+
+setClickListeners();
+
+/*panels.forEach((panel) => {
+  panel.addEventListener("click", function (e) {
+    if (e.target.classList.contains("panel")) {
+      panel.classList.remove("active");
+    }
+  });
+});
 
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
     removeActiveClasses();
     panel.classList.add("active");
   });
-});
-
-function removeActiveClasses() {
-  panels.forEach((panel) => {
-    panel.classList.remove("active");
-  });
-}
+}); */
